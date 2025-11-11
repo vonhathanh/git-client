b40ca707f665747726edad36ea6d842562f25e6a
hanhvn <vonhathanh@gmail.com>
2025-11-11 21:04:27 +0700

    split code into multiple files

---
+++ app/cmd.h
@@ -0,0 +1,9 @@
+#include <string>
+
+using namespace std;
+
+void print_help();
+void print_command(const string &command, const string &guide);
+void print_args(char *args[], string arg_name = "");
+void run_command(char *commands[], char *options[], char *flags[]);
+void parse_arguments(int argc, char *args[]);
